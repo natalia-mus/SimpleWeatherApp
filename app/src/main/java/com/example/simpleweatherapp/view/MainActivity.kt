e138bab5f6db7e8a16dbac1cecd6d24b1360dc2e
natalia-mus <natalia.muskala1ab1@gmail.com>
2021-03-24 20:53:18 +0100

    Showing data

--- app/src/main/java/com/example/simpleweatherapp/view/MainActivity.kt
+++ app/src/main/java/com/example/simpleweatherapp/view/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.example.simpleweatherapp.R
+import com.example.simpleweatherapp.model.Forecast
 import com.example.simpleweatherapp.viewmodel.MainActivityViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -22,8 +24,27 @@ class MainActivity : AppCompatActivity() {
         button_search.setOnClickListener() {
             viewModel.getData(editText_city.text.toString())
         }
+
         viewModel.forecast.observe(
             this,
-            Observer { textView_temperature.text = it!!.main.temperature.toString() })
+            Observer { updateData(it) })
+    }
+
+    private fun updateData(forecast: Forecast) {
+
+        // AndroidMainfest.xml >> aaplication section >> add "android:usesCleartextTraffic="true""
+        Glide.with(this)
+            .load("http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png")
+            .override(80, 80)
+            .into(imageView_icon)
+
+        textView_temperature.text = forecast.main.temperature.toString()
+        textView_description.text = forecast.weather[0].description
+        textView_humidity.text = forecast.main.humidity.toString()
+        textView_pressure.text = forecast.main.pressure.toString()
     }
 }
+
+
+// MVP_simple_sample
+// KotlinMVVM
